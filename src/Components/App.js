4d4f4e42fff4f779d97bba2d914565852a82b341
Kayla Perez <kayla.n.perez404@gmail.com>
2024-08-22 15:07:32 -0700

    put all components into folder

---
+++ src/Components/App.js
@@ -0,0 +1,65 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: 'loading',
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return { ...state, questions: action.payload, status: 'ready' };
+
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+
+    case 'start':
+      return { ...state, status: 'active' };
+    default:
+      throw new Error('Unknown Action');
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status, index } = state;
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataRecieved', payload: data });
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+    fetchQuestions();
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
+        )}
+        {status === 'active' && <Question question={questions[index]} />}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
